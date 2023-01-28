4ccbf3fb1202afb073b4e144935f6ed861e0ccd6
Araya2001 <araya2001@outlook.es>
2023-01-28 17:16:19 -0600

    gitignore update

--- src/main/java/aajdev/io/springjpaspecdemo/AajSpringJpaSpecDemoApplication.java
+++ src/main/java/aajdev/io/springjpaspecdemo/AajSpringJpaSpecDemoApplication.java
@@ -4,12 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
+// TODO: SE DEBE DE MODIFICAR EL ARCHIVO DE PROPIEDADES POR UNO PROPIO
 @SpringBootApplication
-@PropertySource("classpath:env.properties")
+@PropertySource("classpath:env-aaj.properties")
 public class AajSpringJpaSpecDemoApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(AajSpringJpaSpecDemoApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(AajSpringJpaSpecDemoApplication.class, args);
+  }
 }
