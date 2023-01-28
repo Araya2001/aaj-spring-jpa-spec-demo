d31958042c13529905201bf114b5398763068669
Araya2001 <araya2001@outlook.es>
2023-01-28 15:18:35 -0600

    Classpath of properties

--- src/main/java/aajdev/io/springjpaspecdemo/AajSpringJpaSpecDemoApplication.java
+++ src/main/java/aajdev/io/springjpaspecdemo/AajSpringJpaSpecDemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource("classpath:")
+@PropertySource("classpath:env.properties")
 public class AajSpringJpaSpecDemoApplication {
 
 	public static void main(String[] args) {
